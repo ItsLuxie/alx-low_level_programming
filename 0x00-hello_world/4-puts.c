5bbd098088353aeb21bfdaec8974bbe122715da4
ItsLuxie <luxiejones1@gmail.com>
2023-06-15 16:59:45 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,8 @@
 int main(void)
 {
 	char string_name[] = "\"Programming is like building a multilingual puzzle";
-	
+	/*now executing the puts*/
 	puts(string_name);
-
+	/*return*/
       	return (0);
 }
