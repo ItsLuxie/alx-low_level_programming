3a335dd24b5e5290c02ad3b44529fa24529ed501
ItsLuxie <luxiejones1@gmail.com>
2023-06-15 16:43:30 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char string_name[]="\"Programming is like building a multilingual puzzle";
+
+	puts(string_name);
+      
+	return 0;
+}
