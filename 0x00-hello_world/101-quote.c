dbacda955c693611ffaaa38a9345654e0965f2b9
ItsLuxie <luxiejones1@gmail.com>
2023-06-16 08:35:08 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,17 +3,17 @@
 /**
  *main - Entry Point
  *Return: returns 0
- *
  */
 int main(void)
 {
+
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	int count = 0;
 
-		while (count < (int) strlen(str))
-		{
-		putchar(str[count]);			
-		count++;							}
+			while (count < (int) strlen(str))
+						{	
+							putchar(str[count]);
+							count++;								}
 	putchar('\n');
 
 	return (1);
