3c0edd3a3c88180c42692cfd3eb4f96cbe1bc21c
ItsLuxie <luxiejones1@gmail.com>
2023-06-16 09:15:36 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 /**
- *main - Entry Point
- *Return: returns 0
+ * main - Entry Point
+ * Return: returns 0
  */
 int main(void)
 {
-
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	int count = 0;
 
 	while (count < (int) strlen(str))
-		{
-			putchar(str[count]);
-
-			count++;
-		}
+	{
+		putchar(str[count]);
+		count++;
+	}
 	putchar('\n');
-
-	return (1);
+return (1);
 }
