dea064594d3fcae1ebd5725e9a55502f46b54934
ItsLuxie <luxiejones1@gmail.com>
2023-06-16 18:56:14 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,20 +3,17 @@
 /**
  *  * main - Entry Point
  *   * Return: returns 0
- *    */
+ */
 int main(void)
 {
 	char alphabet = 'a';
 	while (alphabet <= 'z')
-    {
-        /* print the value of the variable*/
-        putchar(alphabet);
-
-        /* increment the variable*/
-	 
-        alphabet++;
-    }
-
-    putchar('\n');
-    return (0);
+	{
+	/* print the value of the variable*/
+		putchar(alphabet);
+	/* increment the variable*/ 
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
 }
