54da6d54fa4010e19db4d43658fe99d14aa07f56
ItsLuxie <luxiejones1@gmail.com>
2023-06-17 19:14:24 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+nclude <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (x < 10)
+	{
+		putchar(n + '0');
+			if (x < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			x++;
+	}
+	putchar('\n');
+	return (0);
+}
+
