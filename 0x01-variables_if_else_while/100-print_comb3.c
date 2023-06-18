fac7cb22b18ed37021b14e1df3974c905ff0a43f
ItsLuxie <luxiejones1@gmail.com>
2023-06-17 20:28:57 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y;
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			putchar((x % 10) + '0');putchar((y % 10) + '0');
+			if ((x == 1 && y == 9))
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
