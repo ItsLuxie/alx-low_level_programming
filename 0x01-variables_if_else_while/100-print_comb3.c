029dd8d670dc8ec18a311af229218723c466ba47
ItsLuxie <luxiejones1@gmail.com>
2023-06-17 20:32:44 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,12 +8,14 @@
 int main(void)
 {
 	int x, y;
+
 	for (x = 0; x < 10; x++)
 	{
 		for (y = 0; y < 10; y++)
 		{
-			putchar((x % 10) + '0');putchar((y % 10) + '0');
-			if ((x == 1 && y == 9))
+			putchar((x % 10) + '0');
+			putchar((y % 10) + '0');
+			if ((x == 1 && y == 1))
 				continue;
 			putchar(',');
 			putchar(' ');
