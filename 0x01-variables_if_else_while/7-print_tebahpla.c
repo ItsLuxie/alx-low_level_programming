d8547ce23483f57cd2df2ad0213a8ee8323ca4aa
ItsLuxie <luxiejones1@gmail.com>
2023-06-17 20:03:39 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,20 +4,13 @@
  *  * main - Entry point
  *   *
  *    * Return: Always 0 (Success)
- */
+ *     */
 int main(void)
 {
 	char letter;
 	
-	for (letter = 'z'; letter <= 'z'; letter--)
-	{
+	for (letter = 'z'; letter >= 'a'; letter--)
 		putchar(letter);
-		if (letter == 'a')
-		{
-			break;
-		}
-	}
 	putchar('\n');
-
 	return (0);
 }
