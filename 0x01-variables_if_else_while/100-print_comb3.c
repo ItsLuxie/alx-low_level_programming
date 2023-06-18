daeb0e6444e0b855ca06aaba3eb31b37480822e7
ItsLuxie <luxiejones1@gmail.com>
2023-06-18 04:23:13 -0700

    print two digit combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,25 +4,26 @@
  *  * main - Entry point
  *  *program printing two digit combo 
  *  i is first while j is second digit
+ *Return: Always 0
  */
 int main(void)
 {
-    int i, j;
-
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = i + 1; j <= 9; j++)
-        {
-            putchar(i + '0');
-            putchar(j + '0');
-
-            if (i < 8 || j < 9)
-            {
-            putchar(',');
-            putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
-    return (0);
+	int i, j;
+	
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			
+			if (i < 8 || j < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
