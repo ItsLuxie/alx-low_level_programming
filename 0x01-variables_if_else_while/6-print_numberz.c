c3aee3193725582f3877ddd0260283b8ed715272
ItsLuxie <luxiejones1@gmail.com>
2023-06-17 19:47:57 -0700

    putchar numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	while (x < 10)
+	{
+		putchar(x + '0');
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
