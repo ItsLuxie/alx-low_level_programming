f90b273881cc8dc088c5ed8001edc68e5aa8a6e2
ItsLuxie <luxiejones1@gmail.com>
2023-06-21 06:16:04 -0700

    alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,16 @@
 #include "main.h"
-#include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_alphabet - Entry point
+ * Return:0
  */
 void print_alphabet(void)
 {
-	char alph = 'a';
-
-	while (alph <= 'z')
-	{
-		putchar(alph);
-		alph++;
-	}
-	putchar('\n');
-	return (0);
+        char c = 'a';
+        while (c <= 'z')
+        {
+                _putchar(c);
+                c++;
+        }
+        _putchar('\n');
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,13 @@
-#include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
- * main - check the code
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	print_alphabet();
+	printf("_putchar\n");
 	return (0);
 }
