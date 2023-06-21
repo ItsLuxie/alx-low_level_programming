21d2c9681d0675549cf51b25c1e4d0884f107d81
ItsLuxie <luxiejones1@gmail.com>
2023-06-21 14:48:25 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * print_last_d/igit - return last digit
+ * @n: number
+ *
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int last;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	last = n % 10;
+
+	return (last)
+}
