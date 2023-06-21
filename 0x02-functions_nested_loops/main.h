6160dd6ea8df48b8cea90a0053cae85e008d4967
ItsLuxie <luxiejones1@gmail.com>
2023-06-21 07:59:17 -0700

    main and putchar files

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (success)
- */
-int main(void)
-{
-	printf("_putchar\n");
-	return (0);
-}
+#ifndef main_h
+#define main_h
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+void jack_bauer(void);
+void times_table(void);
+int print_last_digit(int);
+int add(int, int);
+
+#endif
