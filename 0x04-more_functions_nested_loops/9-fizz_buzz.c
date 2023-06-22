b4c5204b4244b61bb21c79c8f835ce5930175ccd
ItsLuxie <luxiejones1@gmail.com>
2023-06-22 14:27:41 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,14 +16,17 @@ int main(void)
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("FizzBuzz");
+			printf(" ");
 		}
 		else if (i % 3 == 0)
 		{
 			printf("Fizz");
+			printf(" ");
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
+			printf(" ");
 		}
 		else
 		{
