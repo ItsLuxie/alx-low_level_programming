8fb972a7c9ab3218a7d8252cd9fab1a6e98105ce
ItsLuxie <luxiejones1@gmail.com>
2023-06-22 12:02:33 -0700

    uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,19 @@
 #include <stdio.h>
 
 /**
- * isupper- check if upper
+ * _isupper - check if upper
  * @c: character.
  *
- * Return: Always 0.
+ * Return: Always 1 if uppercase and 0 else.
  */
 int _isupper(int c)
 {
 	if (isupper(c) == 0)
+	{
 		return (0);
+	}
 	else
+	{
 		return (1);
+	}
 }
