321b972812a5043a69e3981a61f611acd5f65e6b
ItsLuxie <luxiejones1@gmail.com>
2023-06-21 17:01:20 -0700

    .

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,24 +7,24 @@
  */
 void print_to_98(int n)
 {
-	for (; n <= 98; n++)
+	for (; n < 98; n++)
 	{
 		if (n != 98)
 		{
-			_putchar(n + '0');
+			_putchar(n);
 			_putchar(',');
 			_putchar(' ');
 		}
 		else
 		{
-			_putchar(n + '0');
+			_putchar(n);
 		}
 	}
-	for(; n > 98; n--)
+	for (; n > 98; n--)
 	{
 		if (n != 98)
 		{
-			_putchar(n + '0');
+			_putchar(n);
 			_putchar(',');
 			_putchar(' ');
 		}
