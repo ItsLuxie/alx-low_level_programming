c022448f85b3e011403eb7ba4280fededbec1306
ItsLuxie <luxiejones1@gmail.com>
2023-06-22 05:37:23 -0700

    sum of multiple of 3s and 5s

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,15 +8,24 @@ int main(void)
 {
 	int n, sum;
 
+	unsigned long int sum3, sum5;
+
 	sum = 0;
+	sum3 = 0;
+	sum5 = 0;
 
 	for (n = 0; n < 1024; n++)
 	{
-		if ((n % 3 || n % 5) == 0)
+		if ((n % 3) == 0)
 		{
-			sum = sum + n;
-			printf("%d", sum);
+			sum3 = sum3 + n;
+		}
+		else if ((n % 5) == 0)
+		{
+			sum5 = sum5 + n;
 		}
 	}
+	sum = sum3 + sum5;
+	printf("%d\n", sum);
 	return (0);
 }
