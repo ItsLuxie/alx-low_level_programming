17cf037ed95040964bc9542c3fab504fd4c94c9b
ItsLuxie <luxiejones1@gmail.com>
2023-06-22 10:29:44 -0700

    check if uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * isupper- check if upper
+ * @c: character.
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (isupper(c) == 0)
+		return (0);
+	else
+		return (1);
+}
