ad00e0ca7bb5786036b2c5d9bd44b4bbb47a5138
ItsLuxie <luxiejones1@gmail.com>
2023-06-22 13:27:05 -0700

    14 * 10

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - Entry point
+ *
+ * Description: prints 1 to 14 10 times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, count;
+
+	for (count = 0; count <= 10; count++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+				_putchar(i / 10+ 48);
+			_putchar(i % 10 + 48);
+		}
+	}
+	_putchar(10);
+}
