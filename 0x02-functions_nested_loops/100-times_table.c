5d9607126ec8f49b181df17de312ec17cd1292a9
ItsLuxie <luxiejones1@gmail.com>
2023-06-22 10:16:53 -0700

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,51 +1,50 @@
 #include "main.h"
+
 /**
-* print_times_table - prints the n times table, starting with 0
-* @n: number of the times table
-* Return: always 0
+ * print_times_table - Entry point
+ *
+ * Description: prints the n times table, staring with 0
+ *
+ * @n: is the size to timetable
+ *
+ * Return: void
 */
+
 void print_times_table(int n)
 {
-	int i, j, k;
+	int i;
+	int j;
+	int k;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (i = 0 ; i <= n ; i++)
+		for (i = 0; i <= n; i++)
 		{
-			for (j = 0 ; j <= n ; j++)
+			_putchar(48);
+			for (j = 1; j <= n; j++)
 			{
-				k = j * i;
+				_putchar(44);
+				_putchar(32);
+				k = i * j;
 
-				if (j == 0)
+				if (k <= 9)
+					_putchar(32);
+
+				if (k <= 99)
+					_putchar(32);
+
+				if (k >= 100)
 				{
-					_putchar(k + '0');
-				}
-				else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				}
-				else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-				else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
+					_putchar((k / 100) + 48);
+					_putchar((k / 10) % 10 + 48);
+				} else if (k <= 99 && k >= 10)
+					_putchar((k / 10) + 48);
+
+				_putchar((k % 10) + 48);
 			}
-			_putchar('\n');
+			_putchar(10);
 		}
+
 	}
 }
+
