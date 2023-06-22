d0a34636e4566370548f2f93d8fc0521ece16968
ItsLuxie <luxiejones1@gmail.com>
2023-06-22 12:35:45 -0700

    multiplies two digits

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies 2 digits
+ *
+ * Description: multiples two integral digits
+ *
+ * @a: first digit
+ * @b: second digit
+ *
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
