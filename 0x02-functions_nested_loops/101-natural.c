6deaf1536c02b188a705e33a98764ba9887bf974
ItsLuxie <luxiejones1@gmail.com>
2023-06-21 17:56:24 -0700

    sum of 3s and 5s

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,9 +10,9 @@ int main(void)
 
 	sum = 0;
 
-	for (n = 3; n < 1024; n + 3)
+	for (n = 0; n < 1024; n++)
 	{
-		for (n = 5; n < 1024; n + 5)
+		if ((n % 3 || n % 5) == 0)
 		{
 			sum = sum + n;
 			printf("%d", sum);
