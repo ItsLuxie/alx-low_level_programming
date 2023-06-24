287a605cd91bb3d11de488f6c5f33ddb22df0c5e
ItsLuxie <luxiejones1@gmail.com>
2023-06-23 21:51:47 -0700

    triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,14 +8,22 @@
  */
 void print_triangle(int size)
 {
+	int i, j;
+
 	if (size <= 0)
-	{
-		_putchar('\n');
-	}
+		_putchar(10);
 	else
 	{
-		_putchar('#');
-		size--;
+		for (i = 1; i <= size; ++i)
+		{
+			for (j = 1; j <= size; ++j)
+			{
+				if ((i + j) <= size)
+					_putchar('\n');
+				else
+					_putchar(35);
+			}
+			_putchar(10);
+		}
 	}
-	_putchar('\n');
 }
