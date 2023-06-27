9c147250ddc08999cce7c67cc248a528e7d577d2
ItsLuxie <luxiejones1@gmail.com>
2023-06-26 18:58:51 -0700

    printts a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _puts - prints a string
+ * @str: string to be printed
+ */
+void _puts(char *str)
+{
+	str[MAX];
+
+	_putchar(str[MAX]);
+	_putchar('\n');
+}
