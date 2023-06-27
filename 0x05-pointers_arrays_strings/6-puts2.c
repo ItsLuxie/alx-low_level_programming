218764b5c5cb88eef53d906940f5528c81811317
ItsLuxie <luxiejones1@gmail.com>
2023-06-26 20:02:10 -0700

    half a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - prints each charactee in a string
+ * @str: string to be printed
+ */
+void puts2(char *str)
+{
+}
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - prints half of a string 
+ * @str - string to be printed
+ */
+void puts_half(char *str)
+{
+	unsigned int i = ((strlen(str) - 1) / 2) + 1;
+
+	if (strlen(str) % 2 == 0)
+		i = strlen(str) / 2;
+
+	for (; i < strlen(str); i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
+
