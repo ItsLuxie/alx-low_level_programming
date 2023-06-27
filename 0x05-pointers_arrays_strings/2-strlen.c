3c569006d5270e133877d900a69b2d85c905b8fe
ItsLuxie <luxiejones1@gmail.com>
2023-06-26 18:47:07 -0700

    stringlength

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen - a function in c whuch calculate stringlength
+ *
+ * @s: string to be calculated
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
