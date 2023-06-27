9ae0b54635011a7a0018f501acdf49e25c4812c3
ItsLuxie <luxiejones1@gmail.com>
2023-06-26 20:04:16 -0700

    printung half string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <string.h>
 /**
- * puts_half - prints half of a string 
- * @str - string to be printed
+ * puts_half - prints half of a string
+ * @str: string to be printed
  */
 void puts_half(char *str)
 {
