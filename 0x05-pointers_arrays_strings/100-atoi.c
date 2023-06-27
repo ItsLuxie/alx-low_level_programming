09c8bf32c372d8ee25dc99e2bc2871a3a0d8d563
ItsLuxie <luxiejones1@gmail.com>
2023-06-27 12:46:11 -0700

    converting string to int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * _atoi - converts a sring to an integer
+ * @s: source string
+ * Return: 0
+ */
+int _atoi(char *s)
+{
+	int n = atoi(s);
+
+	printf("%d\n", n);
+	return (0);
+}
