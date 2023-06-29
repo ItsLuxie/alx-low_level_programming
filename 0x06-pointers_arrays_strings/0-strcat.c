d390971bf8a263d5715ca2937cc4eb24153123b0
ItsLuxie <luxiejones1@gmail.com>
2023-06-29 13:25:04 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,8 @@
 #include "main.h"
 #include <string.h>
-#include  <stdio.h>
 
 /**
  * *_strcat - concatenates strings
- * Description: copies one string to another to complete sentence
  * @dest: resulting string
  * @src: appended string
  * Return: pointer
@@ -15,13 +13,16 @@ char *_strcat(char *dest, char *src)
 	int srclen = 0;
 	int i;
 
-	if (dest[destnlen] != 0)
+	while (dest[destnlen] != 0)
 		destnlen++;
-	if (src[srclen] != 0)
+
+	while (src[srclen] != 0)
 		srclen++;
+
 	for (i = 0; i < srclen; i++)
 	{
 		dest[destnlen + i] = src[i];
 	}
+
 	return (dest);
 }
