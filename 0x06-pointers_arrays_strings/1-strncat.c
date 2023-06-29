71defccee1e0d87f49d37b16c53242b9e85409a6
ItsLuxie <luxiejones1@gmail.com>
2023-06-29 13:54:36 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strncat - copies specific number of words in a string
+ * @dest: string to be copied to
+ * @src: string to be ammended
+ * @n: size of string to be copied
+ * Return: pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int destnlen = 0;
+	int srclen = 0;
+	int i;
+
+	while (dest[destnlen] != 0)
+		destnlen++;
+	while (src[srclen] == 0)
+		break;
+	for (i = 0; i < n; i++)
+	{
+		dest[destnlen + i] = src[i];
+	}
+	dest[destnlen + i] = 0;
+	return (dest);
+}
