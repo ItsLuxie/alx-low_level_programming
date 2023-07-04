fff05ad3286313f07de209b25e6e571e6037816c
ItsLuxie <luxiejones1@gmail.com>
2023-07-03 19:46:54 -0700

    5 copystring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strstr - locates a substring
+ * @haystack: one string
+ * @needle: another string
+ * Return: pointer
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (needle[i] == hastack[j])
+			{
+				return (needle);
+			}
+		}
+	}
+	return (NULL);
+}
