7d173e35c5d32ea0db94b37f92e3c4b8586936fe
ItsLuxie <luxiejones1@gmail.com>
2023-07-03 17:47:00 -0700

    printing from c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - locates a charcater in a string
+ * @s: the string
+ * @c: the character
+ * Return: s1
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		if (*s == 'c')
+		{
+			return (s);
+		}
+	}
+	return (*s);
+}
