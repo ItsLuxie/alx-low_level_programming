047f6c680ab3a96d5c8b428a47634a13666e6974
ItsLuxie <luxiejones1@gmail.com>
2023-07-04 16:12:33 -0700

    duag sum

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,12 +11,10 @@ void print_diagsums(int *a, int size)
 	int i;
 	int sd1 = 0, sd2 = 0;
 
-	for (i = 0; i < (size * size); i++)
+	for (i = 0; i < size; i++)
 	{
-		if (i % (size + 1) == 0)
-			sd1 += a[i];
-		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
-			sd2 += a[i];
+		sd1 += a[i][i];
+		sd2 += a[i][size - i - 1];
 
 		printf("%d, %d\n", sd1, sd2);
 	}
