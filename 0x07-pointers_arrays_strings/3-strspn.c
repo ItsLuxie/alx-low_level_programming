bb9712eecf7d4b1e883788fffa79fa95ab35c6f3
ItsLuxie <luxiejones1@gmail.com>
2023-07-04 13:25:47 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,14 +8,16 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
+	unsigned int len = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
-		{
-			if (s[i] == accept[j])
-				return (s + i);
-		}
+		for (j = 0; accept[j] != '\0' && accept[j] != s[i]; j++)
+			;
+		if (s[i] == accept[j])
+			len++;
+		if (accept[j] == '\0')
+			return (len);
 	}
-	return ('\0');
+	return (len);
 }
