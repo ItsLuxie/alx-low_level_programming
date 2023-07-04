fa1be3d02f336c0521f1cc7ea3156694f6f2c275
ItsLuxie <luxiejones1@gmail.com>
2023-07-04 15:48:21 -0700

    diagonal sum

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,14 +7,15 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
-	unsigned int sd1 = 0, sd2 = 0;
+	int sd1 = 0, sd2 = 0;
 
 	for (i = 0; i < n; i++)
 	{
 		sd1 += a[i][i];
 		sd2 += a[i][size - i - 1];
 
-		return (sd1, sd2);
+		printf("%d, %d", sd1, sd2);
+		printf("\n");
 	}
 	return ('\0');
 }
