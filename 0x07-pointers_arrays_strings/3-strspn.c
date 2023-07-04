b08082fccb9217c387b3d76136502a09751a5c52
ItsLuxie <luxiejones1@gmail.com>
2023-07-04 10:59:17 -0700

    length of string

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,24 +1,21 @@
 #include "main.h"
-#include <string.h>
 /**
  * _strspn - gets length of a prefix substring
  * @s: string 1
  * @accept: string 2
- * Return: prefix_len
+ * Return: string
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j, prefix_len = 0;
+	unsigned int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
-				prefix_len++;
-			if (accept[j] == '\0')
-				return (prefix_len);
+				return (s + i);
 		}
 	}
-	return (prefix_len);
+	return ('\0');
 }
