023e96a3a968b11958118b7a708df24dd06e4416
ItsLuxie <luxiejones1@gmail.com>
2023-07-04 18:36:19 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,8 +7,24 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	char *result;
+	int i;
+	int j;
 
-	result = strstr(haystack, needle);
-	return (result);
+	if (needle[0] == '\0')
+		return (haystack);
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+					break;
+			}
+			if (needle[j] == '\0')
+				return (haystack + i);
+		}
+	}
+	return ('\0');
 }
