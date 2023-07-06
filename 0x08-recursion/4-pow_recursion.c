cb10754345a39f44f2d0b1e2c86fcfc30559af1d
ItsLuxie <luxiejones1@gmail.com>
2023-07-05 17:02:54 -0700

    power recusion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _pow_recursion - prints power of
+ * @x: number
+ * @y: power
+ * Return: 0 succcess
+ */
+int _pow_recursion(int x, int y)
+{
+	if (x == 1)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	else
+		return (x * _pow_recursion(x, y-1));
+}
