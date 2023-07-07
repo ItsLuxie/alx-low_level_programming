9440794507b5d9e64c09fccb59cbf0ae6330f19d
ItsLuxie <luxiejones1@gmail.com>
2023-07-07 13:07:06 -0700

    c rograms

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,19 @@
 #include "main.h"
 /**
+ * sr - func name
+ * @n: func arg 1
+ * @c: func arg 2
+ * Return: int
+ */
+int sqrt(int n, int c)
+{
+	if ((c * c) == n)
+		return (c);
+	if (c > n)
+		return (-1);
+	return (sqrt(n, c + 1));
+}
+/**
  * _sqrt_recursion - squareroot
  * @n: number
  * Return: 0 success
@@ -7,16 +21,10 @@
 int _sqrt_recursion(int n)
 {
 	int i;
-	int j = 0;
 
-	i = n / 2;
-
-	if (n % 10 == 2 || n % 10 == 3 || n % 10 == 7 || n % 10 == 8)
-		return (-1);
-	while (i != j)
-	{
-		j = i;
-		i = (n / j + j) / 2;
-	}
-	return (i);
+	if (n == 1)
+		i = 1;
+	else
+		i = 2;
+	return (sqrt(n, i));
 }
