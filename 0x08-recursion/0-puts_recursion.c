c86f074ba28c39780c2d011d04f3ea5d1035d732
ItsLuxie <luxiejones1@gmail.com>
2023-07-07 13:12:44 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,10 +6,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	char *p = s;
+
+	if (*p != '\0')
 	{
-		printf("%c", *s);
-		_puts_recursion(*s + 1);
+		_putchar(*p);
+		p++;
+		_puts_recursion(p);
 	}
 	else
 	{
