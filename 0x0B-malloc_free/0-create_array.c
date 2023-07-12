1260e7c5e9987be952426f97edc8d54308b4868f
ItsLuxie <luxiejones1@gmail.com>
2023-07-12 14:46:22 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * create_array - creation of array
+ * @size: size of array
+ * @char: array
+ * Return: 0 or 1
+ */
+char *create_array(unsigned int size, char c)
+{
+	return (0);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+starting with dynamic mem allocation
