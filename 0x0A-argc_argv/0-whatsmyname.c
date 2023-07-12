3e30017b4d6040b9722cb7f739e0404552254cec
ItsLuxie <luxiejones1@gmail.com>
2023-07-12 09:07:17 -0700

    mynameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point with parameters
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 or 1
+ */
+int main(unsigned int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+i have now known about the argc and argvs now lets put to practise
