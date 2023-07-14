02f79e0988834ab3ed287bc0d315ba86a4e37560
ItsLuxie <luxiejones1@gmail.com>
2023-07-14 16:25:27 -0700

    argument to string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,1 +1,12 @@
+#include "main.h"
+#include  <stdlib.h>
+#include <stdio.h>
+/**
+ * str_concat- concatenating strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: null if a string is null
+ */
+char *str_concat(char *s1, char *s2)
+{
 
