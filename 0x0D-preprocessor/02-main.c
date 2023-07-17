8c64ea639acf5daff2d0070b733c6813f7b18f8a
ItsLuxie <luxiejones1@gmail.com>
2023-07-17 12:42:29 -0700

    printfilename

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argumemt vector
+ * Return: 0 success
+ */
+int main()
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
