9a4ecd8c664d9bcbf1038e05fccdfee1b814b669
ItsLuxie <luxiejones1@gmail.com>
2023-07-17 05:05:32 -0700

    commented

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,17 @@
-#include  <stdio.h>
-/* defining the templatenof dog struct*/
+#ifndef STDIO_H
+#define STDIO_H
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
