a5f7b4bdc4f71b28ca861de964f9f800be77095e
ItsLuxie <luxiejones1@gmail.com>
2023-07-19 14:15:58 -0700

    search index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,11 +14,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	while (i < size)
 	{
-		if (cmp(array[i]))
+		if (cmp(array[x]))
 		{
 			return (i);
 		}
-		i++;
+		x++;
 	}
 	return (-1);
 }
