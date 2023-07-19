d0ec7203440292875099b0aa87c9d29b4b748776
ItsLuxie <luxiejones1@gmail.com>
2023-07-19 15:44:02 -0700

    printname

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,5 @@
-#include <stddef.h>
-#ifndef STDIO_H
-#define STDIO_H
+#ifndef STDDEF_H
+#define STDDEF_H
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
