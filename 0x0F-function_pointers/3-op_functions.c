da7d069ec057acfb0732ed23b2f32a797dbdac9b
ItsLuxie <luxiejones1@gmail.com>
2023-07-19 14:54:09 -0700

    just two functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * op_add - adds teo numbers
+ * @a: first no
+ * @b: second number
+ * Return: 0 success
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - entry poiny
+ * @a: first number
+ * @b: second number
+ * Return: 0 success
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - mulriplies numbers
+ * @a: first no
+ * @b: second number
+ * Return: 0 success
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - checks division of number
+ * @a: first number
+ * @b: second number
+ * Return: 0 suceess
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - checks the modulus
+ * @a: first number
+ * @b: sevond number
+ * Return: 0 success
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
