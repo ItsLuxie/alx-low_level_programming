e0434c9ad024b8a827b24e45f0f2a6c06d14c9c2
ItsLuxie <luxiejones1@gmail.com>
2023-07-19 13:45:09 -0700

    parameter of each element

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - function ro give parameter
+ * @array: the array
+ * @size: size of the array
+ * @action: pointer to functionof type char
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array != 0 || action != 0)
+		for (i = 0; i < size; i++)
+			action(array[i]);
+}
