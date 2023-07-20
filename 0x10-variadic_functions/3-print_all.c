e42816c5d800cf8469d2a38ad54b591c31c65738
ItsLuxie <luxiejones1@gmail.com>
2023-07-20 15:56:27 -0700

    all format

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,10 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_all - prints all formats
+ * @format: consist of list of format to use
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+}
