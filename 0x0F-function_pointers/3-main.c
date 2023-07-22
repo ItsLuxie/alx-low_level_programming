884cd5f7d2b1f86253a50f16fc78ea8340fb355a
ItsLuxie <luxiejones1@gmail.com>
2023-07-22 08:44:47 -0700

    update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,37 +1,27 @@
-#include "function_pointers.h"
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * main - entry point with paramters
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 success
+ * main - ...
+ * @argc: ...
+ * @argv: ...
+ * Return: ...
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
-	char *p;
+	int (*oprt)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	num1 = atoi(argv[1]);
-	p = argv[2];
-	num2 = atoi(argv[3]);
-
-	if (!get_op_func(p))
+	oprt = get_op_func(argv[2]);
+	if (!oprt)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*p == '/' && num2 == 0) || (*p == '%' && num2 == 0))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	printf("%d\n", get_op_func(p)(num1, num2));
+	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
