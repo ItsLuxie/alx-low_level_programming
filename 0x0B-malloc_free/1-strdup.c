ac25f510397a6865ee9a23af9c5ed7c7c361bfa3
ItsLuxie <luxiejones1@gmail.com>
2023-08-08 15:50:41 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - leaves a string unto7ched to work on the other
+ * @str: string
+ * Return: str
+ */
+char *_strdup(char *str)
+{
+	int size;
+	char *st;
+
+	if (str == NULL)
+		return (NULL);
+	size = strlen(str);
+	st = malloc(sizeof(char) * size);
+
+	free(st);
+	return (strdup(str));
+}
