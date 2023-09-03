00d014d6ced967f929745a6d68bc8dc0d69fc0c9
Luxie <luxiejones1@gmail.com>
2023-09-04 01:12:17 +0300

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,9 +4,7 @@
 
 /**
 * print_list - function that prints all elements of a list_t list
-*
 * @h: singly linked list to print
-*
 * Return: number of nodes in the list
 */
 size_t print_list(const list_t *h)
