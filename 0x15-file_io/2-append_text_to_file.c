de14f98983acb4a4a5e07ea975781e829cc4b7be
ItsLuxie <luxiejones1@gmail>
2023-11-02 23:10:36 -0700

    update

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,60 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include "main.h"
+/**
+ * append_text_to_file - apends text to file
+ * @filename: path to file
+ * @text_content: content
+ * Return: 1 or -1
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file;
+	ssize_t wfile;
+	int size;
+
+	if (!filename)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_APPEND);
+
+	if (file == -1)
+		return (-1);
+
+	if (!text_content)
+	{
+		close(file);
+		return (1);
+	}
+
+	size = _strlen(text_content);
+	wfile = write(fd, text_content, size);
+
+	if (wfile == -1)
+	{
+		close(file);
+		return (-1);
+	}
+	close(file);
+	return (1);
+}
+
+/**
+ * _strlen - len
+ *
+ * @s: is a pointer to a char
+ *
+ * Return: Always 0.
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (*(s + no) != '\0')
+	{
+		no++;
+	}
+
+	return (no);
+}
